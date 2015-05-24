7dddb8e4e3f2251b54dc52d332cc13b630ad961a
rmxhaha <code000123@gmail.com>
2015-05-24 20:41:13 +0700

    adding platform model

--- js/bwsModel.js
+++ js/bwsModel.js
@@ -6,3 +6,103 @@
 * Author : rmxhaha 
 *********************************/
 
+var Time = function () {
+	this.time = new Date();
+	this.reset = function () {
+		var out = this.getElapsedTime();
+		this.time = new Date();
+
+		return out;
+	}
+
+	this.getElapsedTime = function () {
+		return new Date() - this.time;
+	}
+
+	this.reset();
+}
+
+var RayCast = function( option ){
+	// adaptation from : http://gamedev.stackexchange.com/questions/18436/most-efficient-aabb-vs-ray-collision-algorithms
+
+	// ray starting position
+	var rx = option.x;
+	var ry = option.y;
+	
+	// ray direction
+	var dx = option.tx - option.x;
+	var dy = option.ty - option.y;
+
+	var walls = option.walls;
+	var callback = option.callback || function(){};
+	
+	// ray length
+	var r = Math.sqrt( dx * dx + dy * dy );
+	
+	// direction fraction 1 / normalize( vector )
+	var dfx = r/dx;
+	var dfy = r/dy;
+	
+	var range = r;
+	var wall = false;
+
+	for( var i = 0; i < walls.length; ++ i ){
+		var p = walls[i];
+		var t1 = ( p.x - rx ) * dfx;
+		var t2 = ( p.x + p.width - rx ) * dfx;
+
+		var t3 = ( p.y - ry ) * dfy;
+		var t4 = ( p.y - p.height - ry ) * dfy;
+		
+		var tmin = Math.max( Math.min(t1,t2), Math.min(t3,t4) );
+		var tmax = Math.min( Math.max(t1,t2), Math.max(t3,t4) );
+		
+		// if tmax < 0, ray (line) is intersecting AABB, but whole AABB is behing us
+		if (tmax < 0){
+			continue;
+		}
+		
+		// if tmin > tmax, ray doesn't intersect AABB
+		if (tmin > tmax){
+			continue;
+		}
+		
+		// finding first object to collide with the ray
+		if( tmin < range ){
+			range = tmin;
+			wall = p;
+			
+			callback({ range : tmin, wall : p });
+		}
+	}
+	
+	return { range : range, wall : wall };
+}
+
+var Platform = Backbone.Model.extend({
+	defaults : {
+		x : 0,
+		y : 0,
+		width : 0,
+		penetrable : true
+		/**
+		 *  @param penetrable
+		 *  	penetrable from above, when player request to fall 
+		 *  	if the current platform is penetrable he can fall
+		 */
+	}
+	/**
+		If you are looking for `height` variable.
+		The platform is only impenetrable from above 
+		so the height is only for drawing purposes
+		so it doesn't exist here anymore
+	*/
+});
+
+
+
+
+var World = Backbone.Model.extend({
+});
+
+
