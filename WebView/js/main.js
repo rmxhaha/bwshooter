26ae6fa3e7b0ffd88c37b4c2139a4f0e4acaee35
rmxhaha <code000123@gmail.com>
2015-06-11 22:59:19 +0700

    adjustCanvas function added

--- WebView/js/main.js
+++ WebView/js/main.js
@@ -9,16 +9,20 @@ require.config({
 
 require(['Engine/Game/Player','Engine/View/PlayerView'],function( Player, PlayerView ){
 	var p = new Player({
-		x : 100
+		x : 940,
+		y : - 100,
+		type : 1
 	});
 	
 	var canvas = document.getElementById('canvas');
-	canvas.width = 1000;
-	canvas.height = 500;
-	
 	var context = canvas.getContext('2d');
 	
+	adjustCanvas( canvas );
 	
 	PlayerView.render( context, p );
 });
 
+function adjustCanvas( canvas ) {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
