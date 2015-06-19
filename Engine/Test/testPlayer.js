975348aa39fdfc2cd8b604800e1e2722bc5ba76b
rmxhaha <code000123@gmail.com>
2015-06-19 22:25:30 +0700

    adding more player class test

--- Engine/Test/testPlayer.js
+++ Engine/Test/testPlayer.js
@@ -11,10 +11,12 @@ requirejs.config({
 
 requirejs(['Engine/Utility/class','Engine/Game/Player','Engine/Game/KeyAction'], function( class_, Player, KeyAction ){
 	var p = new Player;
+	var p2 = new Player;
 	
 	// simulate the coupling 
 	var world = { gravity : 1000 };	
 	p.world = world;
+	p2.world = world;
 	
 	var keyAction = new KeyAction({
 		left : true,
@@ -24,9 +26,23 @@ requirejs(['Engine/Utility/class','Engine/Game/Player','Engine/Game/KeyAction'],
 		shoot : false
 	});
 	
+	console.log('--- test going left ---');
 	console.log( keyAction );
 	
 	console.log( p.x );
 	p.update( keyAction, 1 );
 	console.log( p.x );
+	
+	console.log('--- test binary convert ---');
+	console.log( p );
+	p2.parseBin( p.toBin() );
+	console.log( p2 );
+
+
+	console.log('--- test dead ---');
+	console.log( p.isDead );
+	p.die();
+	console.log( p.isDead );
+	
+	
 });
