ff6347965fba353addb8decb28cf7e680a2f8eed
rmxhaha <code000123@gmail.com>
2015-07-11 15:31:17 +0700

    WorldView basic view render

--- Engine/View/WorldView.js
+++ Engine/View/WorldView.js
@@ -1,6 +1,23 @@
 define([], function(){
-	var worldRenderer = function( world ){
+	function renderBackground(){
 		
+	}
+	
+	
+	var worldRenderer = function( ctx, world ){
+		ctx.save();
+
+		// clearing the screen with black
+		ctx.fillStyle = "black";
+		ctx.fillRect( 0, 0, window.innerWidth, window.innerHeight );
+		
+		// camera 
+		ctx.translate( world.camera_x, world.camera_y );
+		
+		
+		
+		
+		ctx.restore();
 	}
 	
 	return { render : worldRenderer }
