93b56ce3310c9b6c57a23ef1072ef319b40fb4ed
rmxhaha <code000123@gmail.com>
2015-09-08 13:21:45 +0700

    adding converter to KeyAction

--- Engine/Game/KeyAction.js
+++ Engine/Game/KeyAction.js
@@ -1,4 +1,4 @@
-define([], function(){
+define(['Engine/Utility/Converter'], function( Converter ){
 	var defaults = {
 		left : false,
 		right : false,
@@ -11,6 +11,26 @@ define([], function(){
 	var KeyAction = function( opt ){
 		_.extend( this, defaults );
 		_.extend( this, opt );
+	}
+	
+	var converter = new Converter.ClassConverter({
+		left : Converter.type.BOOLEAN,
+		right : Converter.type.BOOLEAN,
+		jump : Converter.type.BOOLEAN,
+		sprint : Converter.type.BOOLEAN,
+		shoot : Converter.type.BOOLEAN,
+		fall : Converter.type.BOOLEAN
+	});
+	
+	KeyAction.converter = converter
+	
+	KeyAction.prototype = {
+		getBin : function(){
+			return converter.convertToBin( this );
+		},
+		parseBin : function( bin ){
+			_.extend( this, converter.convertToClass( bin ) );
+		}
 	}
 	
 	return KeyAction;
